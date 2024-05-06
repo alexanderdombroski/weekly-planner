a6dfe2cb0b06385a5fd4e97fa7ddb0d25c73f658
alexanderdombroski <dom21013@byui.edu>
2024-05-06 10:34:11 -0600

    Initial Commit

---
+++ weeklyPlanner/weeklyPlannerApp.swift
@@ -0,0 +1,32 @@
+//
+//  weeklyPlannerApp.swift
+//  weeklyPlanner
+//
+//  Created by Alex Dombroski on 5/6/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct weeklyPlannerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
